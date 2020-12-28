2715e60c6dc8a68a67263756fbf97bc2e8596c3b
Koby <koby412@hotmail.com.tw>
2020-12-28 16:05:40 +0800

    WebApi CRUD

---
+++ prjWebApi/Controllers/CustomerController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using prjWebApi.Models;
+
+namespace prjWebApi.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        dbCustomerEntities db = new dbCustomerEntities();
+
+        // GET: api/Customer
+        public List<tCustomer> Get()
+        {
+            var customer = db.tCustomer;
+            return customer.ToList();
+        }
+
+        // GET: api/Customer/5
+        public tCustomer Get(int fid)
+        {
+            var customer = db.tCustomer
+                             .Where(m => m.fId == fid)
+                             .FirstOrDefault();
+            return customer;
+        }
+
+        // 新增
+        // POST: api/Customer
+        public int Post(string fname,string fphone,string femail,string faddress)
+        {
+            int num = 0;
+
+            try
+            {
+                tCustomer customer = new tCustomer();
+                customer.fName = fname;
+                customer.fPhone = fphone;
+                customer.fEmail = femail;
+                customer.fAddress = faddress;
+                db.tCustomer.Add(customer);
+                num = db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                num = 0;
+            }
+
+            return num;
+        }
+
+        // 修改
+        // PUT: api/Customer/5
+        public int Put(int fid,string fname,string fphone,string femail,string faddress)
+        {
+            int num = 0;
+
+            try
+            {
+                var customer = db.tCustomer
+                                 .Where(m => m.fId == fid)
+                                 .FirstOrDefault();
+                customer.fName = fname;
+                customer.fPhone = fphone;
+                customer.fEmail = femail;
+                customer.fAddress = faddress;
+                num = db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                num = 0;
+            }
+
+            return num;
+        }
+
+        // 刪除
+        // DELETE: api/Customer/5
+        public int Delete(int fId)
+        {
+            int num = 0;
+
+            try
+            {
+                var customer = db.tCustomer
+                                 .Where(m => m.fId == fId)
+                                 .FirstOrDefault();
+                db.tCustomer.Remove(customer);
+                num = db.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                num = 0;
+            }
+
+            return num;
+        }
+    }
+}
---
+++ prjWebApi/Scripts/CRUD.js
@@ -0,0 +1,139 @@
+﻿$(document).ready(function () {
+    var apiurl = "https://localhost:44327/api/Customer";
+
+    $("#btnCreate").on("click", fnCreate);
+    $("#btnEdit").on("click", fnEdit);
+    $("#btnDelete").on("click", fnDelete);
+
+    fnLoadData();
+    function fnLoadData() {
+        $.ajax({
+            url: apiurl,
+            type: 'GET',
+            success: function (data) {
+                $("#tableshow").empty();
+
+                for (var i = 0; i < data.length; i++) {
+                    $("#tableshow").append
+                        (
+                            "<tr>" +
+                            "<td>" + data[i].fId + "</td>" +
+                            "<td>" + data[i].fName + "</td>" +
+                            "<td>" + data[i].fPhone + "</td>" +
+                            "<td>" + data[i].fEmail + "</td>" +
+                            "<td>" + data[i].fAddress + "</td>" +
+                            "<td><input type='button' value='選取' id='btnSelect" + i + "' class='btn btn-info' /></td>" +
+                            "</tr>"
+                        );
+                    $("#btnSelect" + i).on("click", { fid: data[i].fId }, fnSelectData);
+                }
+
+                $("#fid").val("");
+                $("#fname").val("");
+                $("#fphone").val("");
+                $("#femail").val("");
+                $("#faddress").val("");
+            }
+        });
+    }
+
+    //選取某一筆資料
+    function fnSelectData(event) {
+        var fid = event.data.fid;
+
+        $.ajax({
+            url: apiurl + "?fid=" + encodeURI(fid),
+            type: 'GET',
+            success: function (data) {
+                $("#fid").val(data.fId);
+                $("#fname").val(data.fName);
+                $("#fphone").val(data.fPhone);
+                $("#femail").val(data.fEmail);
+                $("#faddress").val(data.fAddress);
+            }
+        });
+    }
+
+    //新增
+    function fnCreate() {
+        var r = confirm("確定要新增嗎?");
+
+        if (r == true) {
+            var fname, fphone, femail, faddress;
+            fname = $("#fname").val();
+            fphone = $("#fphone").val();
+            femail = $("#femail").val();
+            faddress = $("#faddress").val();
+
+            var data = "?fname=" + fname + "&fphone=" + fphone + "&femail=" + femail + "&faddress=" + faddress;
+            
+            $.ajax({
+                url: apiurl + encodeURI(data),
+                type: 'POST',
+                success: function (result) {
+                    if (result != 0) {
+                        alert("新增成功");
+                        fnLoadData();
+                    }
+                    else {
+                        alert("新增失敗");
+                    }
+                }
+            });
+        }
+    }
+
+    //編輯
+    function fnEdit() {
+        var r = confirm("確定要修改嗎");
+
+        if (r == true) {
+            var fid, fname, fphone, femail, faddress;
+            fid = $("#fid").val();
+            fname = $("#fname").val();
+            fphone = $("#fphone").val();
+            femail = $("#femail").val();
+            faddress = $("#faddress").val();
+
+            var data = "?fid=" + fid + "&fname=" + fname +
+                "&fphone=" + fphone + "&femail=" + femail +
+                "&faddress=" + faddress;
+
+            $.ajax({
+                url: apiurl + encodeURI(data),
+                type: 'PUT',
+                success: function (result) {
+                    if (result != 0) {
+                        alert("修改成功");
+                        fnLoadData();
+                    } else {
+                        alert("修改失敗");
+                    }
+                }
+            })
+        }
+    }
+
+    //刪除
+    function fnDelete() {
+        var r = confirm("確定要刪除嗎?");
+
+        if (r == true) {
+            var fid = $("#fid").val();
+
+            $.ajax({
+                url: apiurl + "?fid=" + encodeURI(fid),
+                type: 'DELETE',
+                success: function (result) {
+                    if (result != 0) {
+                        alert("刪除成功");
+                        fnLoadData();
+                    } else {
+                        alert("刪除失敗");
+                    }
+                }
+            });
+        }
+    }
+});
+
